4e75e5ed27c0c2c61742f60779fd3bbe9187b42c
Hintee <dan.hintea@recognos.ro>
2016-07-18 17:31:52 +0300

    OD

---
+++ H.Skeepy/H.Skeepy.Core/Individual.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace H.Skeepy.Core
+{
+    public class Individual
+    {
+        private readonly Guid id;
+        private readonly string firstName;
+        private readonly string lastName;
+
+        public static Individual Existing(Guid id, string firstName, string lastName)
+        {
+            return new Individual(id, firstName, lastName);
+        }
+
+        public static Individual New(string firstName, string lastName)
+        {
+            return new Individual(Guid.NewGuid(), firstName, lastName);
+        }
+
+        private Individual(Guid id, string firstName, string lastName)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public Guid Id { get { return id; } }
+    }
+}
