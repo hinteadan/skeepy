be7c0c47c27834e7ca8d4e96794435757783af0f
Hintee <dan.hintea@recognos.ro>
2017-07-18 12:06:53 +0300

    OD: Define Clash

---
+++ H.Skeepy/H.Skeepy.Model/Clash.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.Model
+{
+    public class Clash<PointType> where PointType : Point
+    {
+        private readonly string id;
+
+        private readonly ReadOnlyDictionary<string, Party> partiesDictionary;
+        private readonly Party[] parties;
+
+        public static Clash<PointType> New(params Party[] parties)
+        {
+            return new Clash<PointType>(Guid.NewGuid().ToString(), parties);
+        }
+
+        public static Clash<PointType> Existing(string id, params Party[] parties)
+        {
+            return new Clash<PointType>(id, parties);
+        }
+
+        private Clash(string id, params Party[] parties)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new InvalidOperationException("A Clash must have an ID");
+            }
+
+            if(!parties.Any())
+            {
+                throw new InvalidOperationException("A Clash must have at least one party");
+            }
+
+            this.id = id;
+            this.parties = parties;
+            partiesDictionary = new ReadOnlyDictionary<string, Party>(parties.ToDictionary(x => x.Id));
+        }
+
+        public string Id { get { return id; } }
+    }
+}
---
+++ H.Skeepy/H.Skeepy.Testicles.Model/ClashOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using H.Skeepy.Model;
+
+namespace H.Skeepy.Testicles.Model
+{
+    [TestClass]
+    public class ClashOperations
+    {
+        [TestMethod]
+        public void Clash_MustHaveAtLeastOneParty()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() => Clash<Point>.New());
+        }
+
+        [TestMethod]
+        public void ExistingClash_MustHaveId()
+        {
+            var party = Party.New("Fed", Individual.New("Fed"));
+            Assert.ThrowsException<InvalidOperationException>(() => Clash<Point>.Existing(null, party));
+            Assert.ThrowsException<InvalidOperationException>(() => Clash<Point>.Existing(string.Empty, party));
+            Assert.ThrowsException<InvalidOperationException>(() => Clash<Point>.Existing("  \t", party));
+
+        }
+    }
+}
