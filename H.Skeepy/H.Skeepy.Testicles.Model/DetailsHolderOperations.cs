bf01ab35aae465e0f4ec0e80baa68f98d085ddc3
Hintee <dan.hintea@recognos.ro>
2017-07-18 07:30:48 +0300

    Unit Test DetailsHolder

---
+++ H.Skeepy/H.Skeepy.Testicles.Model/DetailsHolderOperations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using H.Skeepy.Model;
+using FluentAssertions;
+
+namespace H.Skeepy.Testicles.Model
+{
+    [TestClass]
+    public class DetailsHolderOperations
+    {
+        [TestMethod]
+        public void DetailsHolder_HasNoDetailsToStartWith()
+        {
+            var fed = Individual.New("Roger", "Federer");
+            fed.Details.Should().HaveCount(0);
+        }
+
+        public void DetailsHolder_CanCrudDetails()
+        {
+            var fed = Individual.New("Roger", "Federer");
+            fed.SetDetail("Height", "1.85m");
+            fed.Details.Should().HaveCount(1);
+            fed.GetDetail("Height").Should().Be("1.85m");
+            fed.ZapDetail("Height");
+            fed.Details.Should().HaveCount(0);
+        }
+    }
+}
