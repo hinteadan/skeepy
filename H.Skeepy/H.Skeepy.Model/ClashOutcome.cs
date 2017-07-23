cd7eed932e4b2836642b2d01bbd710654ab46fc9
Hintee <hintea_dan@yahoo.co.uk>
2017-07-23 18:30:09 +0300

    OD: Model Clash Outcome

---
+++ H.Skeepy/H.Skeepy.Model/ClashOutcome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.Model
+{
+    public class ClashOutcome
+    {
+        private readonly Clash clash;
+
+        public ClashOutcome(Clash clash)
+        {
+            this.clash = clash ?? throw new InvalidOperationException("The Clash Outcome must have an underlying clash");
+        }
+
+        public object HasWinner { get { return true; } }
+    }
+}
---
+++ H.Skeepy/H.Skeepy.Testicles.Model/ClashOutcomeOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using H.Skeepy.Model;
+using FluentAssertions;
+
+namespace H.Skeepy.Testicles.Model
+{
+    [TestClass]
+    public class ClashOutcomeOperations
+    {
+        private static readonly Clash clash = Clash.New(Party.New("Fed", Individual.New("Fed")), Party.New("Rafa", Individual.New("Rafa")));
+
+        [TestMethod]
+        public void ClashOutcome_MustHaveAnUnderlyingClash()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() => new ClashOutcome(null));
+        }
+
+        [TestMethod]
+        public void ClashOutcome_CanBeWonByOneParticipatingParty()
+        {
+            var outcome = new ClashOutcome(clash);
+            outcome.HasWinner.Should().Be(true);
+        }
+    }
+}
