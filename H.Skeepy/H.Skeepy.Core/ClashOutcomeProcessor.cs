9d69c0041888f4f5ac5876626df22e82ff4526d9
Hintee <dan.hintea@recognos.ro>
2017-07-24 10:26:40 +0300

    Scaffold ClashOutcome Processor

---
+++ H.Skeepy/H.Skeepy.Core/ClashOutcomeProcessor.cs
@@ -0,0 +1,21 @@
+﻿using H.Skeepy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.Core
+{
+    public abstract class ClashOutcomeProcessor
+    {
+        protected readonly ICanPlaybackPoints pointsPool;
+
+        public ClashOutcomeProcessor(ICanPlaybackPoints pointsPool)
+        {
+            this.pointsPool = pointsPool ?? throw new InvalidOperationException("Points pool must be provided");
+        }
+
+        public abstract ClashOutcome ProcessOutcome();
+    }
+}
