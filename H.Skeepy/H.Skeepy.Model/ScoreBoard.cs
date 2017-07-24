42e2a50d0b09f2b69baa50599cd69c89a11c83be
Hintee <dan.hintea@recognos.ro>
2017-07-24 15:51:32 +0300

    Score board data model

---
+++ H.Skeepy/H.Skeepy.Model/ScoreBoard.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.Model
+{
+    public class ScoreBoard<T> : DetailsHolder
+    {
+        private readonly Clash clash;
+        private readonly ConcurrentDictionary<Party, T> scorePerParty;
+        private (Party, T)[] cachedScores = new(Party, T)[0];
+
+        public ScoreBoard(Clash clash)
+        {
+            this.clash = clash ?? throw new InvalidOperationException("The ScoreBoard must have an underlying clash");
+            scorePerParty = new ConcurrentDictionary<Party, T>(this.clash.Participants.ToDictionary(x => x, x => default(T)));
+        }
+
+        public T this[Party p]
+        {
+            get
+            {
+                ValidateParty(p);
+                return scorePerParty[p];
+            }
+        }
+
+        public (Party, T)[] Scores
+        {
+            get
+            {
+                CheckScoresCache();
+                return cachedScores;
+            }
+        }
+
+        public ScoreBoard<T> SetScore(Party forParty, T score)
+        {
+            ValidateParty(forParty);
+            scorePerParty.AddOrUpdate(forParty, score, (x, y) => score);
+            return this;
+        }
+
+        private void ValidateParty(Party party)
+        {
+            if (!clash.Participants.Contains(party))
+            {
+                throw new InvalidOperationException("The party must be part of the clash");
+            }
+        }
+        private void CheckScoresCache()
+        {
+            if (cachedScores.Length == scorePerParty.Count)
+            {
+                return;
+            }
+
+            cachedScores = scorePerParty.Select(x => (x.Key, x.Value)).ToArray();
+        }
+
+    }
+}
---
+++ H.Skeepy/H.Skeepy.Testicles.Model/ScoreBoardOperations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using H.Skeepy.Model;
+using FluentAssertions;
+
+namespace H.Skeepy.Testicles.Model
+{
+    [TestClass]
+    public class ScoreBoardOperations
+    {
+        private static readonly Party fed = Party.New("Fed", Individual.New("Fed"));
+        private static readonly Party rafa = Party.New("Rafa", Individual.New("Rafa"));
+        private static readonly Clash clash = Clash.New(fed, rafa);
+
+        [TestMethod]
+        public void ScoreBoard_MustHaveUnderlyingClash()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() => new ScoreBoard<int>(null));
+        }
+
+        [TestMethod]
+        public void ScoreBoard_HoldsScorePartiesThatArePartOfTheClash()
+        {
+            var board = new ScoreBoard<int>(clash);
+            var stan = Party.New("Stan", Individual.New("Stan"));
+            Assert.ThrowsException<InvalidOperationException>(() => board.SetScore(stan, 10));
+        }
+
+        [TestMethod]
+        public void ScoreBoard_HoldsScoreForEachParty()
+        {
+            var board = new ScoreBoard<int>(clash);
+            board[fed].Should().Be(0);
+            board.SetScore(fed, 10)[fed].Should().Be(10);
+            board.SetScore(rafa, 8);
+            board.Scores.Should().BeEquivalentTo((fed, 10), (rafa, 8));
+        }
+    }
+}
