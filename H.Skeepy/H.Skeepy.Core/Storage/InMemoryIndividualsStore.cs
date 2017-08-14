f532b10157fd65de514dc2c0a337062e889c0327
Hintee <hintea_dan@yahoo.co.uk>
2017-08-14 23:46:40 +0300

    OD: InMemory Individuals Store

---
+++ H.Skeepy/H.Skeepy.Core/Storage/InMemoryIndividualsStore.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using H.Skeepy.Model;
+using System.IO;
+using System.Collections.Concurrent;
+using System.Runtime.Serialization.Formatters.Binary;
+using H.Skeepy.Model.Storage;
+
+namespace H.Skeepy.Core.Storage
+{
+    public class InMemoryIndividualsStore : IDisposable
+    {
+        private readonly ConcurrentDictionary<string, MemoryStream> storageSpace = new ConcurrentDictionary<string, MemoryStream>();
+
+        public bool Any()
+        {
+            return false;
+        }
+
+        public async Task Put(Individual fed)
+        {
+            await Task.Run(() =>
+            {
+                var stream = new MemoryStream();
+                new BinaryFormatter().Serialize(stream, fed.ToDto());
+                storageSpace.AddOrUpdate(fed.Id, stream, (x, y) => stream);
+            });
+        }
+
+        public async Task<Individual> Get(string id)
+        {
+            return await Task.Run(() => {
+                storageSpace[id].Seek(0, SeekOrigin.Begin);
+                return ((IndividualDto)new BinaryFormatter().Deserialize(storageSpace[id])).ToSkeepy();
+            });
+        }
+
+        public void Dispose()
+        {
+            foreach (var memory in storageSpace)
+            {
+                memory.Value.Dispose();
+            }
+        }
+    }
+}
---
+++ H.Skeepy/H.Skeepy.Testicles.Core/Storage/IndividualsStoreOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using H.Skeepy.Core.Storage;
+using FluentAssertions;
+using H.Skeepy.Model;
+
+namespace H.Skeepy.Testicles.Core.Storage
+{
+    [TestClass]
+    public class IndividualsStoreOperations
+    {
+        private InMemoryIndividualsStore store;
+
+        [TestInitialize]
+        public void Init()
+        {
+            store = new InMemoryIndividualsStore();
+        }
+
+        [TestCleanup]
+        public void Uninit()
+        {
+            store.Dispose();
+        }
+
+        [TestMethod]
+        public void IndividualsStore_IsEmptyByDefault()
+        {
+            store.Any().Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void IndividualsStore_CanSaveAndRetrieveAnIndividual()
+        {
+            var fed = Individual.New("Roger", "Federer");
+            fed.SetDetail("Rank", "1").SetDetail("Country", "SWZ");
+
+            store.Put(fed).Wait();
+            store.Get(fed.Id).Result.ShouldBeEquivalentTo(fed);
+        }
+    }
+}
