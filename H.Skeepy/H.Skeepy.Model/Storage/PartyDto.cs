5a344873a0e31d800179316aa63b6db51e492137
Hintee <dan.hintea@recognos.ro>
2017-08-14 16:14:01 +0300

    Scaffold PartyDto model

---
+++ H.Skeepy/H.Skeepy.Model/Storage/PartyDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.Model.Storage
+{
+    [Serializable]
+    public class PartyDto : IAmASkeepyDtoFor<Party>
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public DetailsHolderDto DetailsHolder { get; set; } = new DetailsHolderDto();
+
+        public void MorphFromSkeepy(Party model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Party ToSkeepy()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ H.Skeepy/H.Skeepy.Testicles.Model/Storage/PartyDtoOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using H.Skeepy.Model.Storage;
+using System.Linq;
+
+namespace H.Skeepy.Testicles.Model.Storage
+{
+    [TestClass]
+    public class PartyDtoOperations
+    {
+        [TestMethod]
+        public void PartyDto_IsSerializable()
+        {
+            var dto = new PartyDto { Id = "Id", Name = "Fed" };
+            dto.DetailsHolder.Details = new(string, string)[] { ("Detail1", "bla"), ("Detail2", "burp") }
+                .Select(x => new DetailsHolderDto.Entry { Key = x.Item1, Value = x.Item2 }).ToArray();
+
+            SerializableChecker.CheckSerializationInAllFormats(dto);
+        }
+    }
+}
