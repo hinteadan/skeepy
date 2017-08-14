c73ef486a84158156d90d17bb6c35942150ef915
Hintee <dan.hintea@recognos.ro>
2017-08-14 14:07:16 +0300

    OD: Skeepy Storage model

---
+++ H.Skeepy/H.Skeepy.Model/Storage/DetailsHolderDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.Model.Storage
+{
+    [Serializable]
+    public class DetailsHolderDto
+    {
+        public KeyValuePair<string, string>[] Details { get; set; }
+    }
+}
---
+++ H.Skeepy/H.Skeepy.Testicles.Model/Storage/DtoOperations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using H.Skeepy.Model.Storage;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using FluentAssertions;
+using Newtonsoft.Json;
+
+namespace H.Skeepy.Testicles.Model.Storage
+{
+    [TestClass]
+    public class DtoOperations
+    {
+        [TestMethod]
+        public void DetailsHolderDto_CanBeSerialized_And_Deserialzied_InBinaryFormat()
+        {
+            var dto = new DetailsHolderDto();
+
+            dto.Should().BeBinarySerializable();
+        }
+
+        [TestMethod]
+        public void DetailsHolderDto_CanBeSerialized_And_Deserialzied_InDataContractFormat()
+        {
+            var dto = new DetailsHolderDto();
+
+            dto.Should().BeDataContractSerializable();
+        }
+
+        [TestMethod]
+        public void DetailsHolderDto_CanBeSerialized_And_Deserialzied_InXmlFormat()
+        {
+            var dto = new DetailsHolderDto();
+
+            dto.Should().BeXmlSerializable();
+        }
+
+        [TestMethod]
+        public void DetailsHolderDto_CanBeSerialized_And_Deserialzied_InJsonFormat()
+        {
+            var dto = new DetailsHolderDto();
+            var reload = JsonConvert.DeserializeObject<DetailsHolderDto>(JsonConvert.SerializeObject(dto));
+            reload.ShouldBeEquivalentTo(dto);
+        }
+    }
+}
