e9fb38c5a76bacddfd49ff5ab8a71917301d17db
Hintee <dan.hintea@recognos.ro>
2017-08-14 15:08:30 +0300

    Remove un-needed XML attributes from DTOs

--- H.Skeepy/H.Skeepy.Model/Storage/DetailsHolderDto.cs
+++ H.Skeepy/H.Skeepy.Model/Storage/DetailsHolderDto.cs
@@ -8,13 +8,11 @@ using System.Xml.Serialization;
 namespace H.Skeepy.Model.Storage
 {
     [Serializable]
-    [XmlRoot("DetailsHolder")]
     public class DetailsHolderDto
     {
         private class ConcreteDetailsHolder : DetailsHolder { }
 
         [Serializable]
-        [XmlType(TypeName = "DetailEntry")]
         public class Entry
         {
             public string Key { get; set; }
--- H.Skeepy/H.Skeepy.Model/Storage/IndividualDto.cs
+++ H.Skeepy/H.Skeepy.Model/Storage/IndividualDto.cs
@@ -8,14 +8,12 @@ using System.Xml.Serialization;
 namespace H.Skeepy.Model.Storage
 {
     [Serializable]
-    [XmlRoot("Individual")]
     public class IndividualDto
     {
         public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        [XmlElement(nameof(DetailsHolder), Type = typeof(DetailsHolderDto))]
         public DetailsHolderDto DetailsHolder { get; set; } = new DetailsHolderDto();
     }
 }
