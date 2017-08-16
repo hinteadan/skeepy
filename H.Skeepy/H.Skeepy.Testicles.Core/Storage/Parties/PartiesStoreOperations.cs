2b31cda037aff2109d4596f0e2e142dca26321e1
Hintee <dan.hintea@recognos.ro>
2017-08-16 11:05:25 +0300

    OD: storage dependecies mechanism

--- H.Skeepy/H.Skeepy.Testicles.Core/Storage/Parties/PartiesStoreOperations.cs
+++ H.Skeepy/H.Skeepy.Testicles.Core/Storage/Parties/PartiesStoreOperations.cs
@@ -11,6 +11,15 @@ namespace H.Skeepy.Testicles.Core.Storage.Parties
         public PartiesStoreOperations(Func<ICanManageSkeepyStorageFor<Party>> storeFactory)
             : base(storeFactory)
         {
+
+        }
+
+        protected IDependOn<Individual> IndividualsDependency
+        {
+            get
+            {
+                return (IDependOn<Individual>)store;
+            }
         }
 
         protected override Party CreateModel()
@@ -19,13 +28,13 @@ namespace H.Skeepy.Testicles.Core.Storage.Parties
         }
 
         [TestMethod]
-        public void PartyStorage_Individuals_Are_Decoupled()
+        public void PartyStorage_Individuals_CanBe_Decoupled()
         {
             var fed = FakeData.GenerateIndividual();
             var rafa = FakeData.GenerateIndividual();
             var party = Party.New("Fedal", fed, rafa);
 
-            (store as IDependOn<Individual>).WithDependency(id => party[id]);
+            IndividualsDependency.WithDependency(id => party[id]);
 
             store.Put(party).Wait();
 
