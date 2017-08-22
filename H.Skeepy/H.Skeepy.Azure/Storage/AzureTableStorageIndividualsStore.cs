de3557d8c39b5f5dcd6241375c381b993a1a795a
Hintee <dan.hintea@recognos.ro>
2017-08-22 16:45:06 +0300

    Scaffold Skeepy Azure module

---
+++ H.Skeepy/H.Skeepy.Azure/Storage/AzureTableStorageIndividualsStore.cs
@@ -0,0 +1,38 @@
+﻿using H.Skeepy.Core.Storage;
+using H.Skeepy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.Azure.Storage
+{
+    public class AzureTableStorageIndividualsStore : ICanManageSkeepyStorageFor<Individual>
+    {
+        public Task<bool> Any()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Individual> Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<LazyEntity<Individual>>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Put(Individual model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
