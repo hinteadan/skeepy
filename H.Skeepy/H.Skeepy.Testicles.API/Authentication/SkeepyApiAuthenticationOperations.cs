3a24786c231e61b0490513b1684c06f5c33caf17
Hintee <hintea_dan@yahoo.co.uk>
2017-08-26 02:27:38 +0300

    Refactor

--- H.Skeepy/H.Skeepy.Testicles.API/Authentication/SkeepyApiAuthenticationOperations.cs
+++ H.Skeepy/H.Skeepy.Testicles.API/Authentication/SkeepyApiAuthenticationOperations.cs
@@ -8,14 +8,13 @@ namespace H.Skeepy.Testicles.API.Authentication
     [TestClass]
     public class SkeepyApiAuthenticationOperations
     {
-        private readonly InMemoryCredentialsAuthenticator authenticator = new InMemoryCredentialsAuthenticator(("hintee", "123qwe"), ("hintee2", "123qwerty"));
+        private readonly ICanAuthenticate<(string,string)> authenticator = new InMemoryCredentialsAuthenticator(("hintee", "123qwe"));
 
         [TestMethod]
         public void SkeepyAuthApi_CanAuthenticateUserViaCredentials()
         {
             authenticator.Authenticate(("asd", "cxcvv")).IsSuccessful.Should().BeFalse();
             authenticator.Authenticate(("hintee", "123qwe")).IsSuccessful.Should().BeTrue();
-            authenticator.Authenticate(("hintee2", "123qwerty")).IsSuccessful.Should().BeTrue();
         }
     }
 }
