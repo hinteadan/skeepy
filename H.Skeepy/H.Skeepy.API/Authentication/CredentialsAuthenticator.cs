5b1de3725b3329f7b47e9ef4cef855109b111668
Hintee <hintea_dan@yahoo.co.uk>
2017-08-26 01:45:35 +0300

    Update nugets

---
+++ H.Skeepy/H.Skeepy.API/Authentication/CredentialsAuthenticator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.API.Authentication
+{
+    public class CredentialsAuthenticator
+    {
+        public object Authenticate(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ H.Skeepy/H.Skeepy.Testicles.API/Authentication/SkeepyApiAuthenticationOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using H.Skeepy.API.Authentication;
+
+namespace H.Skeepy.Testicles.API.Authentication
+{
+    [TestClass]
+    public class SkeepyApiAuthenticationOperations
+    {
+        [TestMethod]
+        public void SkeepyAuthApi_CanAuthenticateUserViaCredentials()
+        {
+            var authenticator = new CredentialsAuthenticator();
+            authenticator.Authenticate("hintee", "123qwe");
+        }
+    }
+}
