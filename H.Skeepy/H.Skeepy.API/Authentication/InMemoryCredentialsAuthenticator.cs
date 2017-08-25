6ef7dd414904ce6ed988c8ffe221a96c8d7170a7
Hintee <hintea_dan@yahoo.co.uk>
2017-08-26 02:05:56 +0300

    OD: Skeepy Authenticator

---
+++ H.Skeepy/H.Skeepy.API/Authentication/InMemoryCredentialsAuthenticator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.API.Authentication
+{
+    public class InMemoryCredentialsAuthenticator
+    {
+        private readonly ReadOnlyDictionary<string, string> users;
+
+        public InMemoryCredentialsAuthenticator(params (string, string)[] users)
+        {
+            this.users = new ReadOnlyDictionary<string, string>(users.ToDictionary(x => x.Item1, x => x.Item2));
+        }
+
+        public bool Authenticate(string username, string password)
+        {
+            return users.ContainsKey(username) && users[username] == password;
+        }
+    }
+}
--- H.Skeepy/H.Skeepy.Testicles.API/Authentication/SkeepyApiAuthenticationOperations.cs
+++ H.Skeepy/H.Skeepy.Testicles.API/Authentication/SkeepyApiAuthenticationOperations.cs
@@ -8,12 +8,14 @@ namespace H.Skeepy.Testicles.API.Authentication
     [TestClass]
     public class SkeepyApiAuthenticationOperations
     {
+        private readonly InMemoryCredentialsAuthenticator authenticator = new InMemoryCredentialsAuthenticator(("hintee", "123qwe"), ("hintee2", "123qwerty"));
+
         [TestMethod]
         public void SkeepyAuthApi_CanAuthenticateUserViaCredentials()
         {
-            var authenticator = new CredentialsAuthenticator();
             authenticator.Authenticate("asd", "cxcvv").Should().BeFalse();
             authenticator.Authenticate("hintee", "123qwe").Should().BeTrue();
+            authenticator.Authenticate("hintee2", "123qwerty").Should().BeTrue();
         }
     }
 }
