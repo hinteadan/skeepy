b7b240cf79ac7256b7402286993dce380043ecc6
Hintee <hintea_dan@yahoo.co.uk>
2017-08-26 01:22:27 +0300

    Scaffold SKeepy API

---
+++ H.Skeepy/H.Skeepy.Testicles.API/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("H.Skeepy.Testicles.API")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("H.Skeepy.Testicles.API")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5bbd7f7b-8a9a-4591-9f29-a9aa4cbcea26")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
