fcfc6ad939f475b8797e13abcdeba2fa315e49fb
Hintee <hintea_dan@yahoo.co.uk>
2017-08-26 20:51:24 +0300

    Scaffold JWT generator

---
+++ H.Skeepy/H.Skeepy.API/Authentication/JsonWebTokenGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.API.Authentication
+{
+    public class JsonWebTokenGenerator : ICanGenerateTokens
+    {
+        public string Generate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ H.Skeepy/H.Skeepy.Testicles.API/Authentication/JsonWebTokenGeneratorOperations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using H.Skeepy.API.Authentication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace H.Skeepy.Testicles.API.Authentication
+{
+    [TestClass]
+    public class JsonWebTokenGeneratorOperations : TokenGeneratorOperations
+    {
+        public JsonWebTokenGeneratorOperations()
+            : base(new JsonWebTokenGenerator())
+        {
+        }
+    }
+}
