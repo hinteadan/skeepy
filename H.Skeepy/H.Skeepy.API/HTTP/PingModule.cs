32ee6b30c25cc169611220883e00d862076944c6
Hintee <hintea_dan@yahoo.co.uk>
2017-08-27 16:48:38 +0300

    OD: Http API

---
+++ H.Skeepy/H.Skeepy.API/HTTP/PingModule.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.API.HTTP
+{
+    public class PingModule : NancyModule
+    {
+        public PingModule()
+            : base("/ping")
+        {
+            Get["/"] = _ => Response.AsText($"pong @ {DateTime.Now}");
+        }
+    }
+}
---
+++ H.Skeepy/H.Skeepy.Testicles.API/HTTP/HttpApiPingOperations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy.Testing;
+using Nancy;
+using FluentAssertions;
+
+namespace H.Skeepy.Testicles.API.HTTP
+{
+    [TestClass]
+    public class HttpApiPingOperations
+    {
+        private readonly Browser browser = new Browser(new DefaultNancyBootstrapper());
+
+        [TestMethod]
+        public void Ping_GetsPongResponse()
+        {
+            var result = browser.Get("/ping", ctx => { ctx.HttpRequest(); });
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            result.ContentType.Should().Be("text/plain");
+            result.Body.AsString().Should().StartWith("pong");
+        }
+    }
+}
