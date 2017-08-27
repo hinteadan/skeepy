b06e890255f98ae6a60a7e6576dbbab166ddf2ae
Hintee <hintea_dan@yahoo.co.uk>
2017-08-27 18:04:14 +0300

    OD: Registration endpoint

---
+++ H.Skeepy/H.Skeepy.API/HTTP/RegistrationModule.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.API.HTTP
+{
+    public class RegistrationModule : NancyModule
+    {
+        public RegistrationModule() 
+            : base("/registration")
+        {
+            Post["/apply"] = _ => HttpStatusCode.Accepted;
+        }
+    }
+}
--- H.Skeepy/H.Skeepy.Testicles.API/HTTP/RegistrationEndpoints.cs
+++ H.Skeepy/H.Skeepy.Testicles.API/HTTP/RegistrationEndpoints.cs
@@ -27,5 +27,22 @@ namespace H.Skeepy.Testicles.API.HTTP
             });
             result.StatusCode.Should().Be(HttpStatusCode.Accepted);
         }
+
+        [TestMethod]
+        public void Registration_ChecksApplicantData()
+        {
+            browser.Post("/registration/apply", x => { x.JsonBody(null as object); })
+                .StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+            browser.Post("/registration/apply", x => { x.JsonBody(new { FirstName = "Roger" }); })
+                .StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+            browser.Post("/registration/apply", x => { x.JsonBody(new { LastName = "Roger" }); })
+                .StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+            browser.Post("/registration/apply", x => { x.JsonBody(new { Email = "Roger" }); })
+                .StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+            browser.Post("/registration/apply", x => { x.JsonBody(new { FirstName = "Roger", LastName = "Federer", Email = "dfasdfas" }); })
+                .StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+            browser.Post("/registration/apply", x => { x.JsonBody(new { FirstName = "Roger", LastName = "Federer", Email = "abc@asdasd" }); })
+               .StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+        }
     }
 }
