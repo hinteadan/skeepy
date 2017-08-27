2bef9d34acd7bd3db3e57c105f8500958b497c2f
Hintee <hintea_dan@yahoo.co.uk>
2017-08-27 23:44:49 +0300

    Scaffold unit tests and thus functionality for Registration Flow

---
+++ H.Skeepy/H.Skeepy.Testicles.API/Registration/RegistrationFlowOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace H.Skeepy.Testicles.API.Registration
+{
+    [TestClass]
+    public class RegistrationFlowOperations
+    {
+        [TestMethod]
+        public void RegistrationFlow_GeneratesTokenForValidatingApplicant()
+        {
+
+        }
+
+        [TestMethod]
+        public void RegistrationFlow_ValidatesApplicantToken()
+        {
+
+        }
+
+        [TestMethod]
+        public void RegistrationFlow_SetsPasswordForValidatedApplicant()
+        {
+
+        }
+
+        [TestMethod]
+        public void RegistrationFlow_MakesApplicantAuthenticateableAndIndividualInSkeepy()
+        {
+
+        }
+    }
+}
