65dbab279a9a84b7a3841c1a52f2c59b6ef4d86a
Hintee <dan.hintea@recognos.ro>
2017-08-28 12:28:45 +0300

    OD: Registration Flow

--- H.Skeepy/H.Skeepy.Testicles.API/Registration/RegistrationFlowOperations.cs
+++ H.Skeepy/H.Skeepy.Testicles.API/Registration/RegistrationFlowOperations.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using H.Skeepy.API.Registration;
 using H.Skeepy.API.Authentication;
 using FluentAssertions;
+using H.Skeepy.API.Authentication.Storage;
 
 namespace H.Skeepy.Testicles.API.Registration
 {
@@ -12,11 +13,14 @@ namespace H.Skeepy.Testicles.API.Registration
         private readonly ICanGenerateTokens<string> tokenGenerator = new JsonWebTokenGenerator();
 
         [TestMethod]
-        public void RegistrationFlow_GeneratesTokenForValidatingApplicant()
+        public void RegistrationFlow_GeneratesAndStoresTokenForValidatingApplicant()
         {
+            var tokenStore = new InMemoryTokensStore();
+
             var registrationToken = new RegistrationFlow(tokenGenerator).Apply(new ApplicantDto { FirstName = "Fed", Email = "hintea_dan@yahoo.co.uk" }).Result;
             registrationToken.Should().NotBeNull();
             registrationToken.UserId.Should().Be("hintea_dan@yahoo.co.uk");
+            tokenStore.Get(registrationToken.Id).Result.ShouldBeEquivalentTo(registrationToken);
         }
 
         [TestMethod]
