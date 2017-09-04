d5f70ecef36133d8da47e5c54bf0c36387be6cf2
Hintee <hintea_dan@yahoo.co.uk>
2017-09-05 00:18:49 +0300

    Implement InMemoryCredentialStore

--- H.Skeepy/H.Skeepy.API/Authentication/Storage/InMemoryCredentialsStore.cs
+++ H.Skeepy/H.Skeepy.API/Authentication/Storage/InMemoryCredentialsStore.cs
@@ -1,5 +1,6 @@
 ﻿using H.Skeepy.Core.Storage;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -8,13 +9,13 @@ using System.Threading.Tasks;
 
 namespace H.Skeepy.API.Authentication.Storage
 {
-    public class InMemoryCredentialsStore : ICanGetSkeepyEntity<Credentials>
+    public class InMemoryCredentialsStore : ICanGetSkeepyEntity<Credentials>, ICanStoreSkeepy<Credentials>
     {
-        private readonly ReadOnlyDictionary<string, Credentials> users;
+        private readonly ConcurrentDictionary<string, Credentials> users;
 
         public InMemoryCredentialsStore(params Credentials[] users)
         {
-            this.users = new ReadOnlyDictionary<string, Credentials>(users.ToDictionary(x => x.Username));
+            this.users = new ConcurrentDictionary<string, Credentials>(users.ToDictionary(x => x.Username));
         }
 
         public void Dispose()
@@ -27,6 +28,18 @@ namespace H.Skeepy.API.Authentication.Storage
             var taskSource = new TaskCompletionSource<Credentials>();
             taskSource.SetResult(users.ContainsKey(id) ? users[id] : null);
             return taskSource.Task;
+        }
+
+        public Task Put(Credentials model)
+        {
+            users.AddOrUpdate(model.Id, model, (x, y) => model);
+            return Task.CompletedTask;
+        }
+
+        public Task Zap(string id)
+        {
+            users.TryRemove(id, out var credentials);
+            return Task.CompletedTask;
         }
     }
 }
