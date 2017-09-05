5ecf5866e3cccc69a0b0c06c83079dd4950ea8cc
Hintee <dan.hintea@recognos.ro>
2017-09-05 19:15:51 +0300

    OD: RegistrationFlow

---
+++ H.Skeepy/H.Skeepy.API/Hasher.cs
@@ -0,0 +1,22 @@
+﻿using CryptoHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.API
+{
+    public sealed class Hasher
+    {
+        public static string Hash(string value)
+        {
+            return Crypto.HashPassword(value);
+        }
+
+        public static bool Verify(string hash, string value)
+        {
+            return Crypto.VerifyHashedPassword(hash, value);
+        }
+    }
+}
---
+++ H.Skeepy/H.Skeepy.Testicles.API/HashOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using H.Skeepy.API;
+using FluentAssertions;
+
+namespace H.Skeepy.Testicles.API
+{
+    [TestClass]
+    public class HashOperations
+    {
+        [TestMethod]
+        public void Hasher_CorrectlyHashesStrings()
+        {
+            var hash = Hasher.Hash("123qwe");
+            hash.Should().NotBeEmpty();
+            hash.Should().NotBe("123qwe");
+            Hasher.Verify(hash, "123qwe").Should().BeTrue();
+        }
+    }
+}
