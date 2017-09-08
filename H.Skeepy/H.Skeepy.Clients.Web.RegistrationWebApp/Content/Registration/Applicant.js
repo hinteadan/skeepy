6d4ae6169243fe09eb38a58e89085268c5c0481c
Hintee <dan.hintea@recognos.ro>
2017-09-08 13:10:29 +0300

    OD: Registration client app

---
+++ H.Skeepy/H.Skeepy.Clients.Web.RegistrationWebApp/Content/Registration/Applicant.js
@@ -0,0 +1,12 @@
+﻿class Applicant {
+
+    get Email() { return this._email || null; }
+    set Email(value) { this._email = value; }
+
+    get FirstName() { return this._firstName || null; }
+    set FirstName(value) { this._firstName = value; }
+
+    get LastName() { return this._lastName || null; }
+    set LastName(value) { this._lastName = value; }
+
+}
