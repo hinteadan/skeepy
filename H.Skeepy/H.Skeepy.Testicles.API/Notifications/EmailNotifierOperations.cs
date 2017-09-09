293eb37c481028cac936bd7556f3c6fc086b9fc5
Hintee <hintea_dan@yahoo.co.uk>
2017-09-09 11:18:40 +0300

    Implement Email notifier

--- H.Skeepy/H.Skeepy.Testicles.API/Notifications/EmailNotifierOperations.cs
+++ H.Skeepy/H.Skeepy.Testicles.API/Notifications/EmailNotifierOperations.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using System.Net.Mail;
 using H.Skeepy.API.Notifications;
+using System.Linq;
+using FluentAssertions;
 
 namespace H.Skeepy.Testicles.API.Notifications
 {
@@ -26,13 +28,20 @@ namespace H.Skeepy.Testicles.API.Notifications
         public void Uninit()
         {
             mailClient.Dispose();
-            Directory.Delete(tmpEmailStorageFolder, true);
+            //Directory.Delete(tmpEmailStorageFolder, true);
         }
 
         [TestMethod]
         public void EmailNotifier_SendsEmail()
         {
             new EmailNotifier(mailClient).Notify(new NotificationDestination("a@a.com", "Test"), "Registration", "Content").Wait();
+            var emails = Directory.EnumerateFiles(tmpEmailStorageFolder);
+            emails.Should().HaveCount(1);
+            var message = File.ReadAllLines(emails.Single());
+            message.Single(x => x.StartsWith("Subject:")).Should().EndWith("Registration");
+            message.Single(x => x.StartsWith("Content-Type:")).Should().Contain("text/html");
+            message.Single(x => x.StartsWith("To:")).Should().Contain("a@a.com");
+            message.Single(x => x.StartsWith("From:")).Should().Contain("no-reply-registration@skeepy.ro").And.Contain("SKeepy Registration");
         }
     }
 }
