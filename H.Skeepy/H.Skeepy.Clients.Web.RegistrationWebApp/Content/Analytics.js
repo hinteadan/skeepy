0d0764894e5430883ead272f95185e13755b8841
Hintee <hintea_dan@yahoo.co.uk>
2017-09-09 10:30:53 +0300

    Add event tracking to Application Insight

---
+++ H.Skeepy/H.Skeepy.Clients.Web.RegistrationWebApp/Content/Analytics.js
@@ -0,0 +1,10 @@
+﻿class Analytics {
+
+    static trackEvent(name, label, value) {
+        if (!name) return;
+        if (window.appInsights) {
+            window.appInsights.trackEvent(name, label, value);
+        }
+    }
+
+}
--- H.Skeepy/H.Skeepy.Clients.Web.RegistrationWebApp/Content/Registration/RegistrationAppViewModel.js
+++ H.Skeepy/H.Skeepy.Clients.Web.RegistrationWebApp/Content/Registration/RegistrationAppViewModel.js
@@ -9,16 +9,20 @@
     submitApplication(formElement) {
         if (!$(formElement).valid()) return;
 
+        Analytics.trackEvent('ApplyForRegistration');
+
         this.isApplicationBeingSubmitted(true);
         this.submitLabel('Submitting, please wait...');
 
         $.post('skeepy/registration/apply', ko.mapping.toJS(this.applicant), (token) => {
+            Analytics.trackEvent('ApplyForRegistrationSucceeded', token);
             window.location.href = `application/success/${token}`;
         })
         .fail(() => {
             this.isApplicationBeingSubmitted(false);
             this.submitLabel('Application failed, please try again');
             setTimeout(() => { this.submitLabel('Apply'); }, 5000);
+            Analytics.trackEvent('ApplyForRegistrationFailed');
         })
         .always(() => { });
     }
