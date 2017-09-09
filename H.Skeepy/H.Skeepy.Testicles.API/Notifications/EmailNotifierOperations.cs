e886bf8daa437a655f5dbfa5c39124d87f96fad5
Hintee <hintea_dan@yahoo.co.uk>
2017-09-09 11:10:58 +0300

    Housekeeping

--- H.Skeepy/H.Skeepy.Testicles.API/Notifications/EmailNotifierOperations.cs
+++ H.Skeepy/H.Skeepy.Testicles.API/Notifications/EmailNotifierOperations.cs
@@ -26,7 +26,7 @@ namespace H.Skeepy.Testicles.API.Notifications
         public void Uninit()
         {
             mailClient.Dispose();
-            //Directory.Delete(tmpEmailStorageFolder, true);
+            Directory.Delete(tmpEmailStorageFolder, true);
         }
 
         [TestMethod]
