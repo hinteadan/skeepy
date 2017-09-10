29a78d9c5b4b7046cabc79ef6258af780aa1c615
Hintee <hintea_dan@yahoo.co.uk>
2017-09-10 20:16:34 +0300

    Refactor Template parser

--- H.Skeepy/H.Skeepy.API/TemplateParser.cs
+++ H.Skeepy/H.Skeepy.API/TemplateParser.cs
@@ -9,22 +9,71 @@ namespace H.Skeepy.API
 {
     public class TemplateParser
     {
+        private class Part
+        {
+            private Part(string text, bool isTag)
+            {
+                Text = text;
+                IsTag = isTag;
+            }
+
+            public readonly string Text;
+            public readonly bool IsTag;
+
+            public static Part Content(string text)
+            {
+                return new Part(text, false);
+            }
+
+            public static Part Tag(string name)
+            {
+                return new Part(name, true);
+            }
+        }
+
         private readonly string template;
+        private readonly Queue<Part> parts;
 
         public TemplateParser(string template)
         {
             this.template = template ?? string.Empty;
+            parts = SplitTemplate(this.template);
+        }
+
+        private static Queue<Part> SplitTemplate(string template)
+        {
+            var queue = new Queue<Part>();
+            var remains = template;
+            while (!string.IsNullOrEmpty(remains))
+            {
+                var a = remains.IndexOf("@{");
+                if(a < 0)
+                {
+                    queue.Enqueue(Part.Content(remains));
+                    break;
+                }
+                var b = remains.IndexOf('}', a + 2);
+                if(b < 0)
+                {
+                    throw new InvalidOperationException($"Invalid tag in template, has no closing bracket. Tag starts at offset ${a}");
+                }
+                queue.Enqueue(Part.Content(remains.Substring(0, a)));
+                queue.Enqueue(Part.Tag(remains.Substring(a+2, b-a-2)));
+                remains = remains.Substring(b+1);
+            }
+            return queue;
         }
 
         public string Compile(params (string, string)[] payload)
         {
-            var result = template;
-            foreach(var tag in payload)
+            var result = new StringBuilder();
+
+            foreach(var part in parts)
             {
-                result = result.Replace($"@{{{tag.Item1}}}", tag.Item2);
+                result.Append(part.IsTag ? payload.SingleOrDefault(x => x.Item1 == part.Text).Item2 ?? string.Empty : part.Text);
             }
-            result = new Regex(@"(@\{[a-zA-Z0-9]+\})").Replace(result, string.Empty);
-            return result;
+
+            return result.ToString();
         }
     }
 }
