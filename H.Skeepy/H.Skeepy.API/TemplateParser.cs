c15897a427d9dee8d7d351f43166bf3425b35e2c
Hintee <hintea_dan@yahoo.co.uk>
2017-09-10 19:40:34 +0300

    OD: Template parser

---
+++ H.Skeepy/H.Skeepy.API/TemplateParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.API
+{
+    public class TemplateParser
+    {
+        private readonly string template;
+
+        public TemplateParser(string template)
+        {
+            this.template = template ?? string.Empty;
+        }
+
+        public string Compile(params (string, string)[] payload)
+        {
+            var result = template;
+            foreach(var tag in payload)
+            {
+                result = result.Replace($"@{{{tag.Item1}}}", tag.Item2);
+            }
+            result = new Regex(@"(@\{[a-zA-Z0-9]+\})").Replace(result, string.Empty);
+            return result;
+        }
+    }
+}
---
+++ H.Skeepy/H.Skeepy.Testicles.API/TemplateParserOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using H.Skeepy.API;
+using FluentAssertions;
+
+namespace H.Skeepy.Testicles.API
+{
+    [TestClass]
+    public class TemplateParserOperations
+    {
+        [TestMethod]
+        public void TemplateParser_LoadsAndReplacesTemplateTagsWithEmptyStringsIfNoReplacementIsGiven()
+        {
+            new TemplateParser("@{Test}").Compile().Should().Be(string.Empty);
+            new TemplateParser("@{Test}bla bla@{blabla}").Compile().Should().Be("bla bla");
+        }
+
+        [TestMethod]
+        public void TemplateParser_CorrectlyReplacesTags()
+        {
+            new TemplateParser("My name is @{Name}").Compile(("Name", "Hintee")).Should().Be("My name is Hintee");
+            new TemplateParser("My name is @{FirstName} @{LastName}").Compile(("FirstName", "Hintea"), ("LastName", "Dan")).Should().Be("My name is Hintea Dan");
+        }
+    }
+}
