0f9a52558c3b3b3b786cf01cad112af75b983c94
Hintee <hintea_dan@yahoo.co.uk>
2017-09-11 23:51:10 +0300

    Scaffold Set Password knockout stuff

---
+++ H.Skeepy/H.Skeepy.Clients.Web.RegistrationWebApp/Content/Registration/SetPasswordApp.js
@@ -0,0 +1,1 @@
+﻿ko.applyBindings(new SetPasswordAppViewModel());
---
+++ H.Skeepy/H.Skeepy.Clients.Web.RegistrationWebApp/Content/Registration/SetPasswordAppViewModel.js
@@ -0,0 +1,9 @@
+﻿class SetPasswordAppViewModel {
+    constructor() {
+        this.password = ko.observable('');
+        this.passwordConfirm = ko.observable('');
+    }
+
+    submitPassword() {
+    }
+}
