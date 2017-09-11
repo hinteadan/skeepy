c408ae80f2aed3828e7a14a62fd8481c074e68eb
Hintee <hintea_dan@yahoo.co.uk>
2017-09-12 00:14:27 +0300

    OD: Set Password page

--- H.Skeepy/H.Skeepy.Clients.Web.RegistrationWebApp/Content/Registration/SetPasswordAppViewModel.js
+++ H.Skeepy/H.Skeepy.Clients.Web.RegistrationWebApp/Content/Registration/SetPasswordAppViewModel.js
@@ -2,6 +2,8 @@
     constructor() {
         this.password = ko.observable('');
         this.passwordConfirm = ko.observable('');
+        this.submitLabel = ko.observable('Set Password');
+        this.isFormBeingSubmitted = ko.observable(false);
     }
 
     submitPassword() {
