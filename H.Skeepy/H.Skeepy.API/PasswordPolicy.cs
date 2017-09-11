aaea782ac8b047464e099dd470f773ed531c394b
Hintee <hintea_dan@yahoo.co.uk>
2017-09-12 01:27:24 +0300

    Define Password Policy Validator

---
+++ H.Skeepy/H.Skeepy.API/PasswordPolicy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.API
+{
+    public class PasswordPolicy
+    {
+        public static void Validate(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new SkeepyApiException("The password cannot be empty");
+            }
+        }
+    }
+}
---
+++ H.Skeepy/H.Skeepy.Testicles.API/PasswordPolicyOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using H.Skeepy.API;
+using FluentAssertions;
+
+namespace H.Skeepy.Testicles.API
+{
+    [TestClass]
+    public class PasswordPolicyOperations
+    {
+        [TestMethod]
+        public void PasswordValidator_DoesntAllowNullOrEmptyPasswords()
+        {
+            new Action(() => { PasswordPolicy.Validate(null); }).ShouldThrow<SkeepyApiException>();
+            new Action(() => { PasswordPolicy.Validate(string.Empty); }).ShouldThrow<SkeepyApiException>();
+            new Action(() => { PasswordPolicy.Validate("   \t "); }).ShouldThrow<SkeepyApiException>();
+        }
+    }
+}
