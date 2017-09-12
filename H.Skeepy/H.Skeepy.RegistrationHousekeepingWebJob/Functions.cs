082f236a6d7595ffcc8a3eb06fb2bdcf4cf0c60d
Hintee <hintea_dan@yahoo.co.uk>
2017-09-12 23:02:05 +0300

    OD: Registration Housekeeping Webjob

---
+++ H.Skeepy/H.Skeepy.RegistrationHousekeepingWebJob/Functions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using H.Skeepy.API.Housekeeping;
+using Nancy.TinyIoc;
+
+namespace H.Skeepy.RegistrationHousekeepingWebJob
+{
+    public class Functions
+    {
+        private static readonly IEnumerable<ImAJanitor> janitors;
+
+        static Functions()
+        {
+            janitors = TinyIoCContainer.Current.Resolve<IEnumerable<ImAJanitor>>();
+        }
+
+        public static void RunHousekeeping([TimerTrigger("00:30:00", RunOnStartup = true)] TimerInfo timerInfo, TextWriter log)
+        {
+            log.WriteLine($"Running Housekeeping on Registration Web App @ {DateTime.Now}");
+            Task.WaitAll(janitors.Select(x => x.Clean()).ToArray());
+            log.WriteLine($"Finished Running Housekeeping on Registration Web App @ {DateTime.Now}");
+        }
+    }
+}
---
+++ H.Skeepy/H.Skeepy.RegistrationHousekeepingWebJob/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using H.Skeepy.API.Infrastructure;
+
+namespace H.Skeepy.RegistrationHousekeepingWebJob
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            DefaultSkeepyApiBuildingBlocks.RegisterWithTinyIoc();
+
+            var config = new JobHostConfiguration
+            {
+                DashboardConnectionString = "DefaultEndpointsProtocol=https;AccountName=hskeepydev;AccountKey=gdhovqMPlUxFcFyLG4G12NnRdceGKu0YNEE+EdX250GgTGBkXUTbttFpwoZk5KjuliFjE1OFJ/KtWtwLr7bw5g==;EndpointSuffix=core.windows.net",
+                StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=hskeepydev;AccountKey=gdhovqMPlUxFcFyLG4G12NnRdceGKu0YNEE+EdX250GgTGBkXUTbttFpwoZk5KjuliFjE1OFJ/KtWtwLr7bw5g==;EndpointSuffix=core.windows.net",
+            };
+            config.UseTimers();
+
+            if (config.IsDevelopment)
+            {
+                config.UseDevelopmentSettings();
+            }
+
+            var host = new JobHost(config);
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+    }
+}
