bde92fd527f0cf1f8afe6905e47418c2048e2b32
Hintee <dan.hintea@recognos.ro>
2017-09-12 13:48:28 +0300

    OD: Token Cleanup service

---
+++ H.Skeepy/H.Skeepy.API/Housekeeping/TokenJanitor.cs
@@ -0,0 +1,25 @@
+﻿using H.Skeepy.API.Authentication;
+using H.Skeepy.Core.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Skeepy.API.Housekeeping
+{
+    public class TokenJanitor
+    {
+        private readonly ICanManageSkeepyStorageFor<Token> tokenStore;
+
+        public TokenJanitor(ICanManageSkeepyStorageFor<Token> tokenStore)
+        {
+            this.tokenStore = tokenStore ?? throw new InvalidOperationException($"Must provide a {nameof(tokenStore)}");
+        }
+
+        public Task Clean()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ H.Skeepy/H.Skeepy.Testicles.API/Housekeeping/TokenHousekeepingOperations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using H.Skeepy.Core.Storage;
+using H.Skeepy.API.Authentication;
+using H.Skeepy.API.Authentication.Storage;
+using H.Skeepy.API.Housekeeping;
+using System.Linq;
+using FluentAssertions;
+
+namespace H.Skeepy.Testicles.API.Housekeeping
+{
+    [TestClass]
+    public class TokenHousekeepingOperations
+    {
+        private readonly JsonWebTokenGenerator expiredTokenGenerator = new JsonWebTokenGenerator(TimeSpan.FromMinutes(-1));
+        private readonly JsonWebTokenGenerator validTokenGenerator = new JsonWebTokenGenerator(TimeSpan.FromHours(1));
+
+        private ICanManageSkeepyStorageFor<Token> tokenStore;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+
+        }
+
+        [TestCleanup]
+        public void TestUninit()
+        {
+            tokenStore?.Dispose();
+        }
+
+        [TestMethod]
+        public void TokenHousekeeping_DoesNothingForValidTokens()
+        {
+            var tokens = new Token[] { validTokenGenerator.Generate("hintee1"), validTokenGenerator.Generate("hintee2") };
+            tokenStore = new InMemoryTokensStore(tokens);
+            new TokenJanitor(tokenStore).Clean().Wait();
+            tokenStore.Get().Result.Select(x => x.Full).ShouldAllBeEquivalentTo(tokens);
+        }
+    }
+}
