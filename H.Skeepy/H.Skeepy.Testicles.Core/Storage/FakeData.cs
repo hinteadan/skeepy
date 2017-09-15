440115ef0ea228fb7570ebca7181ff67030f9ba6
Hintee <hintea_dan@yahoo.co.uk>
2017-09-15 22:45:06 +0300

    Scaffold azure registration store unit tests

--- H.Skeepy/H.Skeepy.Testicles.Core/Storage/FakeData.cs
+++ H.Skeepy/H.Skeepy.Testicles.Core/Storage/FakeData.cs
@@ -1,4 +1,5 @@
-﻿using H.Skeepy.Core.Storage;
+﻿using H.Skeepy.API.Registration.Storage;
+using H.Skeepy.Core.Storage;
 using H.Skeepy.Model;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,7 @@ namespace H.Skeepy.Testicles.Core.Storage
         private static readonly Bogus.DataSets.Commerce commerceGenerator = new Bogus.DataSets.Commerce();
         private static readonly Bogus.DataSets.Name namesGenerator = new Bogus.DataSets.Name();
         private static readonly Bogus.DataSets.Address addressGenerator = new Bogus.DataSets.Address();
+        private static readonly Bogus.DataSets.Internet internetGenerator = new Bogus.DataSets.Internet();
         private static readonly Bogus.Randomizer random = new Bogus.Randomizer();
 
         public static Individual GenerateIndividual()
@@ -25,6 +27,18 @@ namespace H.Skeepy.Testicles.Core.Storage
         public static Party GenerateParty()
         {
             return Party.New(commerceGenerator.ProductName(), Enumerable.Range(0, random.Int(1, 10)).Select(x => GenerateIndividual()).ToArray());
+        }
+
+        public static RegisteredUser GenerateRegisteredUser()
+        {
+            return new RegisteredUser
+            {
+                Email = internetGenerator.Email(),
+                FirstName = namesGenerator.FirstName(),
+                LastName = namesGenerator.LastName(),
+                SkeepyId = Guid.NewGuid().ToString(),
+                Status = RegisteredUser.AccountStatus.PendingValidation
+            };
         }
 
         public static void FillStorage(ICanManageSkeepyStorageFor<Individual> storage, int count = 1000)
---
+++ H.Skeepy/H.Skeepy.Testicles.Core/Storage/Registration/AzureRegistrationStoreOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure.Storage;
+using H.Skeepy.API.Registration.Storage;
+using H.Skeepy.Core.Storage;
+using H.Skeepy.Azure.Storage;
+
+namespace H.Skeepy.Testicles.Core.Storage.Registration
+{
+    [TestClass]
+    public class AzureRegistrationStoreOperations : StoreOperationsBase<RegisteredUser>
+    {
+        private static readonly string connectionString = "DefaultEndpointsProtocol=https;AccountName=hskeepydev;AccountKey=gdhovqMPlUxFcFyLG4G12NnRdceGKu0YNEE+EdX250GgTGBkXUTbttFpwoZk5KjuliFjE1OFJ/KtWtwLr7bw5g==;EndpointSuffix=core.windows.net";
+        private static string collectionName = "SkeepyRegistrationTest";
+
+        private static CloudTableClient tableStoreClient = CloudStorageAccount.Parse(connectionString).CreateCloudTableClient();
+
+        public AzureRegistrationStoreOperations() : base(() => new AzureTableStorageRegistrationStore())
+        {
+        }
+
+        [TestInitialize]
+        public override void Init()
+        {
+            collectionName = $"SkeepyRegistrationTest{Guid.NewGuid()}".Replace("-", string.Empty);
+            base.Init();
+        }
+
+        [TestCleanup]
+        public override void Uninit()
+        {
+            base.Uninit();
+            tableStoreClient.GetTableReference(collectionName).DeleteIfExists();
+        }
+
+        protected override RegisteredUser CreateModel()
+        {
+            return FakeData.GenerateRegisteredUser();
+        }
+    }
+}
