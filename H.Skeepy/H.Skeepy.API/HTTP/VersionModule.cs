4455a377e34ce2425e8229e4cc461b6d405142c1
Hintee <hintea_dan@yahoo.co.uk>
2017-09-18 22:25:20 +0300

    Expose version endpoints

---
+++ H.Skeepy/H.Skeepy.API/HTTP/VersionModule.cs
@@ -0,0 +1,13 @@
+﻿using Nancy;
+
+namespace H.Skeepy.API.HTTP
+{
+    public class VersionModule : NancyModule
+    {
+        public VersionModule() : base($"{SkeepyApiConfiguration.BasePath}/version")
+        {
+            Get["/"] = _ => Response.AsText(Versioning.Version.Self.GetCurrent().ToString());
+            Get["/json"] = _ => Response.AsJson(Versioning.Version.Self.GetCurrent());
+        }
+    }
+}
--- H.Skeepy/H.Skeepy.Clients.Web.RegistrationWebApp/Bootstrapper.cs
+++ H.Skeepy/H.Skeepy.Clients.Web.RegistrationWebApp/Bootstrapper.cs
@@ -1,11 +1,4 @@
 ﻿using H.Skeepy.API;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Nancy.Bootstrapper;
-using Nancy.TinyIoc;
 using H.Skeepy.API.Infrastructure;
 
 namespace H.Skeepy.Clients.Web.RegistrationWebApp
