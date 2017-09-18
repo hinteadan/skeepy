1056d23a8dfe709d628da0814fd742af41a7ef79
Hintee <dan.hintea@recognos.ro>
2017-09-18 11:36:18 +0300

    Scaffold Define Logging Assembly with LogTiming functionality

---
+++ H.Skeepy/H.Skeepy.Logging/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using NLog;
+
+namespace H.Skeepy.Logging
+{
+    public static class Extensions
+    {
+        public static LogTiming Timing(this Logger log, string actionName, LogLevel logLevel)
+        {
+            return new LogTiming(log, actionName, logLevel);
+        }
+
+        public static LogTiming Timing(this Logger log, string actionName)
+        {
+            return new LogTiming(log, actionName);
+        }
+    }
+}
---
+++ H.Skeepy/H.Skeepy.Logging/LogTiming.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NLog;
+
+namespace H.Skeepy.Logging
+{
+    public class LogTiming : IDisposable
+    {
+        private readonly Logger log;
+        private readonly LogLevel logLevel;
+        private readonly string actionName;
+        private DateTime startedAt = DateTime.MinValue;
+        private DateTime endedAt = DateTime.MinValue;
+
+        public LogTiming(Logger log, string actionName, LogLevel logLevel)
+        {
+            this.log = log ?? throw new InvalidOperationException("A logger must be provided");
+            this.logLevel = logLevel;
+            this.actionName = actionName;
+
+            Start();
+        }
+
+        public LogTiming(Logger log, string actionName)
+            : this(log, actionName, LogLevel.Debug)
+        { }
+
+        private void Start()
+        {
+            startedAt = DateTime.Now;
+            log.Log(logLevel, "Running {0} ...", actionName);
+        }
+
+        private void Stop()
+        {
+            endedAt = DateTime.Now;
+            log.Log(logLevel, "Finished {0} in {1}", actionName, endedAt - startedAt);
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+}
