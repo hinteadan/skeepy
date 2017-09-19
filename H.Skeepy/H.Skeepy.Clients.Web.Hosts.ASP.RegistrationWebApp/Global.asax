7385360662a5245baaa58e234b56d1a3c95a27d3
Hintee <dan.hintea@recognos.ro>
2017-09-19 14:08:20 +0300

    Try to fix version file path for azure

---
+++ H.Skeepy/H.Skeepy.Clients.Web.Hosts.ASP.RegistrationWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="H.Skeepy.Clients.Web.Hosts.ASP.RegistrationWebApp.Global" Language="C#" %>
---
+++ H.Skeepy/H.Skeepy.Clients.Web.Hosts.ASP.RegistrationWebApp/Global.asax.cs
@@ -0,0 +1,54 @@
+﻿using H.Versioning.VersionProviders;
+using NLog;
+using System;
+
+namespace H.Skeepy.Clients.Web.Hosts.ASP.RegistrationWebApp
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        static Global()
+        {
+            FileVersionProviderSettings.Default.VersionFilePath = @"D:\home\site\wwwroot\version.txt";
+
+            log.Info($"Configured H.Versioning with version file path: {FileVersionProviderSettings.Default.VersionFilePath}");
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
